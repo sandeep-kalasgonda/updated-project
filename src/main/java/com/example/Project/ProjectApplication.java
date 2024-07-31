7271a85e6bf818d5d1136ccb06754536fa95cd3c
sandeep k <sandeeprajendra00@gmail.com>
2024-07-31 15:44:07 +0530

    new files to tomcat

--- src/main/java/com/example/Project/ProjectApplication.java
+++ src/main/java/com/example/Project/ProjectApplication.java
@@ -2,12 +2,18 @@ package com.example.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectApplication.class);
+	}
 }
--- src/main/java/com/example/Project/controller/AppointmentController.java
+++ src/main/java/com/example/Project/controller/AppointmentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/appointments")
+@RequestMapping("/appointments") // Updated to "/appointments"
 public class AppointmentController {
 
     @Autowired
