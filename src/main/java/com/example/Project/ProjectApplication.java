4bed7a85700fc3c3fd58d95f70c3708d8ed5d583
sandeep k <sandeeprajendra00@gmail.com>
2024-08-05 10:28:26 +0530

    new change for war file

--- src/main/java/com/example/Project/ProjectApplication.java
+++ src/main/java/com/example/Project/ProjectApplication.java
@@ -2,16 +2,9 @@ package com.example.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ProjectApplication.class);
-	}
+public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
